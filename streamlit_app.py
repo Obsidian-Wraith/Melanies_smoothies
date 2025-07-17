ace25f3b69108b6562ed4d1c3fd84193395691e4
Santhosh K <90301766+Obsidian-Wraith@users.noreply.github.com>
2025-07-17 18:12:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 
 # Title and description
@@ -7,9 +8,22 @@ st.title(":cup_with_straw: Customize your smoothie!")
 name_on_order = st.text_input("Name of Smoothie")
 st.write("The name on your order is ", name_on_order)
 
+# Fetch Snowflake connection details from secrets
+snowflake_secrets = st.secrets["snowflake"]
+
+# Creating Snowflake session
+session = Session.builder.configs({
+    "account": snowflake_secrets["account"],
+    "user": snowflake_secrets["user"],
+    "password": snowflake_secrets["password"],
+    "role": snowflake_secrets["role"],
+    "warehouse": snowflake_secrets["warehouse"],
+    "database": snowflake_secrets["database"],
+    "schema": snowflake_secrets["schema"],
+    "client_session_keep_alive": snowflake_secrets["client_session_keep_alive"]
+}).create()
+
 # Fetch fruit options from Snowflake
-cnx=st.connection("snowflake")
-session = cnx.session()
 all_fruits = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).to_pandas()['FRUIT_NAME'].tolist()
 
 # Multi-select for ingredients
@@ -35,3 +49,6 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!, Melly me!!!!!!!!', icon="✅")
         except Exception as e:
             st.error(f"Error occurred: {e}")
+
+# Close the session
+session.close()
